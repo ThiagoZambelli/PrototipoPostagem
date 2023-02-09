e79d0e7b7ade7883bc6e5e87867f93829c1bb71e
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-09 19:32:44 -0300

    Finalização do protótipo, aplicando os filtros de pesquisa nas postagens.

--- app/filtroAutor.js
+++ app/filtroAutor.js
@@ -1,4 +1,39 @@
 let autores = [];
+const inputPesquisa = document.getElementById('filtro-pesquisa')
+const btnFiltro = document.querySelectorAll("[data-filtro-btn]");
+const alertaSemPostagensComTitulo = document.getElementById('sempostagens')
+btnFiltro.forEach(btn => {
+    btn.addEventListener('click', filtrarBtn)
+})
+
+function filtrarBtn(){
+    if(this.value === "pesquisar"){ 
+        let postagemFiltradaTitulo = []
+        postagemFiltradaTitulo.push(postagens.find(post => post.title == inputPesquisa.value));
+        inputPesquisa.value = '';
+        console.log(postagemFiltradaTitulo)
+        if(postagemFiltradaTitulo == ''){
+            alertaSemPostagensComTitulo.classList ='inline__container sempostagens';
+            sectionPosts.innerHTML = "";
+           
+        }else{
+            postar(postagemFiltradaTitulo);
+        }
+        
+    }else if(this.value === "crescente"){
+        let postFiltradosCrescente = postagens.sort((a,b) => a.id - b.id)
+        alertaSemPostagensComTitulo.classList = 'none__container';
+        postar(postFiltradosCrescente);
+    }else if(this.value === "decrecente"){
+        let postFiltradosDecrecente = postagens.sort((a,b) => b.id - a.id)
+        alertaSemPostagensComTitulo.classList = 'none__container';
+        postar(postFiltradosDecrecente);
+    }else if(this.value === "limpar-filtro"){
+        alertaSemPostagensComTitulo.classList = 'none__container';
+        postar(postagens);
+    }
+    
+}
 
 function btnFiltarAutor(){
     autores = document.querySelectorAll("#postagem-autor")
@@ -17,7 +52,7 @@ function filtrarAutor(){
     const idClicado = usuarios.find(item => item.name === autorClicado.innerHTML);
     const postagensFiltradasPorAutor = postagens.filter(post =>{         
         return post.userId === idClicado.id;
-    })
+    })    
     postar(postagensFiltradasPorAutor);    
     
 }
